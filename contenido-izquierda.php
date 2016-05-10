a8bfa486c380d22e7183c0f14a3a6398e2e8d3e9
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-05-10 07:48:14 -0500

    Enlazando botones del contenido de la izquierda
    Se colocaron los enlaces de los botones "Modelo Florecer" y "Descarga mi
    libro"

--- contenido-izquierda.php
+++ contenido-izquierda.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 	<div class="izquierda-contenido">
-		<a href="">
+		<a href="modelo-florecer-donde-estas-plantado.php">
 			<div class="articulos-izquierda">
 				<img class="articulo-imagen1" src="img/modelo-florecer.gif">
 			</div>
@@ -21,12 +21,12 @@
 				<a class="boton-verde" href="">SUSCRIBETE <img src="img/suscribete.png"></a>
 			</div>
 		</a>
-		<a href="">
+		<a href="javascript:void( window.open('http://form.jotformz.com/form/51592129569667', 'blank','scrollbars=yes,toolbar=no,width=700,height=500'))">
 			<div class="articulos-izquierda">
 				<h2>DESCARGA MI<br>LIBRO GRATIS</h2>
 				<img class="articulo-imagen4" src="img/libros.jpg">
 				<div class="clear"></div>
-				<a class="boton-verde" href="">DESCARGALO <img src="img/descargar.png"></a>
+				<a class="boton-verde" href="javascript:void( window.open('http://form.jotformz.com/form/51592129569667', 'blank','scrollbars=yes,toolbar=no,width=700,height=500'))">DESCARGALO <img src="img/descargar.png"></a>
 			</div>
 		</a>
 		<a href="">
